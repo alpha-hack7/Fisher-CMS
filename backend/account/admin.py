258635b40b9ee68444d44d411d34ff478716f779
alpha-hack7 <alpha.andambi@gmail.com>
2026-09-08 11:05:12 +0300

    Change admin.py to allow email input field on django-admin user add

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from .models import User
 
 
-# Register your models here.
 @admin.register(User)
-class UserAdmin(UserAdmin):
-    pass
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                ),
+            },
+        ),
+    )
