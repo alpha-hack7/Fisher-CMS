a1524117461aefec4d29f936174c92f544633d49
alpha-hack7 <alpha.andambi@gmail.com>
2026-09-08 22:57:51 +0300

    Attempt to change anchor to arrow

--- frontend/src/sections/components/nav_arrow.jsx
+++ frontend/src/sections/components/nav_arrow.jsx
@@ -1,5 +1,14 @@
 import Arrow from "../../assets/anchor.png";
-import './components.css';
+// import ArrowLeft as Arrow from "react-feather";
+import "./components.css";
 
-export const RightArrow = ({onClick}) => <div className="arrow right" onClick={onClick}><img src={Arrow} alt="Right Arrow" /></div>;
-export const LeftArrow = ({onClick}) => <div className="arrow left" onClick={onClick}><img src={Arrow} alt="Left Arrow" /></div>;
+export const RightArrow = ({ onClick }) => (
+  <div className="arrow right" onClick={onClick}>
+    <img src={Arrow} alt="Right Arrow" />
+  </div>
+);
+export const LeftArrow = ({ onClick }) => (
+  <div className="arrow left" onClick={onClick}>
+    <img src={Arrow} alt="Left Arrow" />
+  </div>
+);
